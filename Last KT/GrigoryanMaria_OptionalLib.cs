d0fef034081859b3a7608baf27a1a60b15178788
AFK-Shark <graffin.may@yandex.ru>
2024-06-05 19:11:45 +0300

    All code

---
+++ Last KT/GrigoryanMaria_OptionalLib.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrigoryanMaria_OptionalLib
+{
+    public class IncorrectOptionalAccessException : InvalidOperationException
+    {
+        public IncorrectOptionalAccessException() { }
+        public IncorrectOptionalAccessException(string message) : base(message) { }
+        public IncorrectOptionalAccessException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    public interface IOptional<T> where T : struct
+    {
+        T Value { get; set; }
+        void SetValue(T? value);
+        T GetValueOrDefault();
+        bool Empty { get; }
+    }
+
+    public class Optional<T> : IOptional<T> where T : struct
+    {
+        private T? _value;
+
+        public Optional()
+        {
+            _value = null;
+        }
+
+        public Optional(T value)
+        {
+            _value = value;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (Empty)
+                {
+                    throw new IncorrectOptionalAccessException("Параметр не имеет значения.");
+                }
+
+                return (T)_value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public void SetValue(T? value)
+        {
+            if (value.HasValue)
+            {
+                _value = value.Value;
+            }
+            else
+            {
+                _value = null;
+            }
+        }
+
+        public T GetValueOrDefault()
+        {
+            return Empty ? default(T) : (T)_value;
+        }
+
+        public bool Empty => !_value.HasValue;
+
+        public override string ToString()
+        {
+            return Empty ? "empty" : Value.ToString();
+        }
+    }
+
+    public class ExtendedOptional<T> : Optional<T> where T : struct
+    {
+        public event Action<T> OnOptionalFilled;
+        public event Action OnOptionalEmptied;
+
+        public ExtendedOptional() : base() { }
+        public ExtendedOptional(T value) : base(value) { }
+
+        public new T Value
+        {
+            get { return base.Value; }
+            set
+            {
+                base.Value = value;
+                if (!Empty)
+                {
+                    OnOptionalFilled?.Invoke(value);
+                }
+                else
+                {
+                    OnOptionalEmptied?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Last KT/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using GrigoryanMaria_OptionalLib;
+
+namespace GrigoryanMaria_OptionalService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Ведите кол-во объектов для создания: ");
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Некорректный ввод. Введите кол-во объектов снова: ");
+            }
+
+            Optional<int>[] optionals = new Optional<int>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите значение для параметра {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out int value))
+                {
+                    optionals[i] = new Optional<int>(value);
+                }
+                else
+                {
+                    optionals[i] = new Optional<int>();
+                }
+            }
+
+            ExtendedOptional<int> extendedOptional = new ExtendedOptional<int>();
+            extendedOptional.OnOptionalFilled += OnOptionalFilledHandler;
+            extendedOptional.OnOptionalEmptied += OnOptionalEmptiedHandler;
+
+            Console.WriteLine("\nВаши объекты:");
+            foreach (var opt in optionals)
+            {
+                Console.WriteLine(opt.ToString());
+            }
+
+            extendedOptional.SetValue(42);
+            extendedOptional.SetValue(null);
+        }
+
+        static void OnOptionalFilledHandler(int value)
+        {
+            Console.WriteLine($"Заполненное значение: {value}");
+        }
+
+        static void OnOptionalEmptiedHandler()
+        {
+            Console.WriteLine("Пустое значение.");
+        }
+    }
+}
